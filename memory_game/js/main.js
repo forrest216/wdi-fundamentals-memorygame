0a87cdccda62a22bce7ab3cffc0fde6f0d2b4a77
Forrest Smith <forrestsmith@Forrests-MacBook-Air.local>
2019-03-26 00:58:53 -0700

    Created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,22 @@
 console.log("Up and running!");
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
+
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+	console.log('You found a match!');
+	} else {
+	console.log('Sorry, try again!');
+}};
+
+var flipCard = function(cardId){ //card flip confirmation
+	console.log('User flipped ' + cards[cardId]);
+	checkForMatch();
+};
+flipCard(0);
+flipCard(2);
+/*var cardOne = cards[0];
+	var cardTwo = cards[1];
 cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length === 2){
-if (cardsInPlay[0] === cardsInPlay[1]){
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again!');
-}
-}
+cardsInPlay.push(cardTwo);*/
+//if (cardsInPlay.length === 2){
